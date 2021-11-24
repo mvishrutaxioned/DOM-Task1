841fa6725b5530471ed2756353d0cc5d46d8a584
VishrutMargam <vishrutm@axioned.com>
2021-11-24 17:31:19 +0530

    added changing tab functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,25 @@ $(document).ready(() => {
     $('.menu').click(() => $('nav').toggleClass('slide'))
     $('#close').click(() => $('nav').toggleClass('slide'))
 
+    var tabIndex = 1;
+    $('.tabs li a').each(function (index, tab){
+        $(tab).click((e) => {
+            changeTab(e, index+1)
+        })
+    })
+
+    function changeTab(e, n) {
+        e.preventDefault();
+        showTab(tabIndex = n)
+    }
+    
+    function showTab(n) {
+        
+        $('.tabs li a').each(function (index, tab){ $(this).removeClass('active') })
+        $('.tab-content > li').each(function (index, tab){ $(this).hide() })
+
+        $('.tabs li a').eq(n-1).addClass('active')
+        $('.tab-content > li').eq(n-1).fadeIn()
+
+    }
 })
